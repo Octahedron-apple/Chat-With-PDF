da5a49e3b35c1d64fe6610a899026f37e5ca67f7
Octahedron-apple <bhavya1singh1@gmail.com>
2026-05-25 22:08:58 +0530

    made the embedder logic

--- src/pdf_chat/embedder.py
+++ src/pdf_chat/embedder.py
@@ -2,24 +2,31 @@ import os
 from langchain_community.vectorstores import FAISS
 from langchain_core.embeddings import Embeddings
 from langchain_core.documents import Document
-store_path = os.path.expanduser("~/.pdfchat/")
-from langchain_community.vectorstores import FAISS
-from langchain_core.embeddings import Embeddings
-from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
 from langchain_openai import OpenAIEmbeddings
-
-class embedder:
+store_path = os.path.expanduser("~/.pdfchat/")
+class Embedder:
     def get_embedder(self,provider="ollama", model_name="qwen3-embedding:0.6b"):
         if provider == "ollama":
-            return OllamaEmbeddings(model=model_name)
+            return OllamaEmbeddings(model=model_name)    
         if provider == "open_router":
             api_key = os.getenv("OPENROUTER_API_KEY")
+            if not api_key:
+                raise ValueError("Missing Environment Key: Please set 'OPENROUTER_API_KEY'.") 
             return OpenAIEmbeddings(
                 openai_api_key=api_key,
                 openai_api_base="https://openrouter.ai/api/v1",
                 model=model_name
             )
-        raise ValueError("Invalid provider")
-class data_base: 
-    def create_and_save()
+        raise ValueError(f"Invalid provider: {provider}")
+
+class Database: 
+    def create_and_save(self,documents: list[Document], embedder: Embeddings):
+        if not documents: 
+            raise ValueError("No documents provided")        
+        os.makedirs(store_path, exist_ok=True)
+        db = FAISS.from_documents(documents, embedder)
+        db.save_local(store_path)
+        return db
+    def load_db(self,embedder: Embeddings):
+        return FAISS.load_local(store_path, embedder, allow_dangerous_deserialization=True)
